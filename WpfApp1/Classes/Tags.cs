7add880d7d95069510baab4ef28a911aeec511be
acad_4 <acad_4@DESKTOP-AUGO5K5>
2021-06-21 00:05:54 -0700

    teph

---
+++ WpfApp1/Classes/Tags.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WpfApp1.Classes
+{
+    class Tags
+    {
+
+        public class Prediction
+        {
+            public string TagId { get; set; }
+            public string Tag { get; set; }
+            public string Probability { get; set; }
+
+        }
+
+        public class CustomVision
+        {
+            public string Id { get; set; }
+            public string Project { get; set; }
+            public string Iteration { get; set; }
+            public string Created { get; set; }
+            public List<Prediction> Predictions { get; set; }
+        }
+    }
+}
---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.Win32;
+using System.IO;
+using System.Net.Http;
+using Newtonsoft.Json;
+using static WpfApp1.Classes.Tags;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.Filter = "Image files (*.png; *.jpg)|*.png;*.jpg;*jpeg|All Files (*.*)|*.*";
+            dialog.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+
+            if (dialog.ShowDialog() == true)
+            {
+                string fileName = dialog.FileName;
+                selectedImage.Source = new BitmapImage(new Uri(fileName));
+
+                MakePredictionAsync(fileName);
+            }
+        }
+
+        private async void MakePredictionAsync(string fileName)
+        {
+            string url = "https://southcentralus.api.cognitive.microsoft.com/customvision/v1.1.Predict";
+            string prediction_key = "fd63926c323344a0aacaa249ebd73fc6";
+            string content_type = "applicaion/octet-stream";
+            var file = File.ReadAllBytes(fileName);
+
+            using (HttpClient client = new HttpClient())
+            {
+                client.DefaultRequestHeaders.Add("Prediction_Key", prediction_key);
+
+                using(var content = new ByteArrayContent(file))
+                {
+                    content.Headers.ContentType = new System.Net.Http.Headers.MediaTypeHeaderValue(content_type);
+                    var responce = await client.PostAsync(url, content);
+
+                    var responseString = await responce.Content.ReadAsStringAsync();
+
+
+                    List<Prediction> predictions = (JsonConvert.DeserializeObject<CustomVision>(responseString)).Predictions;
+                }
+
+            }
+        }
+    }
+}
